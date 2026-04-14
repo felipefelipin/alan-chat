2b98104cd908298a9824714f1b51058d3e8c8efb
Felipe <felipegomes771a@gmail.com>
2026-04-14 09:24:43 -0300

    fix: nome e status posicionados ao lado da foto + remove bolinha verde

--- public/app.js
+++ public/app.js
@@ -558,10 +558,12 @@ function mountChat() {
 
 <div class="avatarWrap">
   <div class="avatar">
-    <img 
-      src="${ASSETS.avatar}?v=${Date.now()}" 
-      alt="${CONTACT.title}"
-    />
+    <img src="${ASSETS.avatar}?v=1" alt="${CONTACT.title}" />
+  </div>
+
+  <div class="titlebox">
+    <div class="name">${CONTACT.title}</div>
+    <div class="status" id="status">${CONTACT.subtitle}</div>
   </div>
 </div>
 
